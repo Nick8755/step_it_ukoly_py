9fad7240afcdcaa6898fe1e95058bcd39cd44929
Nick8755 <rocl.na85@gmail.com>
2024-11-19 19:02:56 +0100

    add change password function

---
+++ users_change_password.py
@@ -0,0 +1,59 @@
+# users_change_password.py
+
+import os, json
+
+DATA_PATH = 'users.json'
+
+def read_data():
+    with open(DATA_PATH, encoding='utf-8') as file:
+        return json.load(file)
+
+def write_data(data):
+    with open(DATA_PATH, mode="w", encoding='utf-8') as file:
+        json.dump(data, file) # zapís do json
+
+def check_password(password, password_repeat):
+    if password != password_repeat:
+        raise ValueError('Passwords do not match')
+
+def check_username(data, username):
+    if username in data:
+        raise ValueError('User already exists')
+
+def register(username, password, password_repeat):
+    check_password(password, password_repeat)
+    data = read_data()
+    check_username(data, username)
+    data[username] = password
+    write_data(data)
+
+def login(username, password):
+    data = read_data()
+    try:
+        assert data[username] == password, 'Password is incorrect'
+        return True
+    except (KeyError, AssertionError):
+        return False
+#Tady je cast ukolu (change_password) !!!!!!
+def change_password(username, old_password, password, password_repeat):
+    data = read_data()
+    if username not in data:
+        raise ValueError('User does not exist')
+    if data[username] != old_password:
+        raise ValueError('Old password is incorrect')
+
+    check_password(password, password_repeat)
+    data[username] = password
+    write_data(data)
+
+
+def delete_user(username, password):
+    data = read_data()
+    if username in data and data[username] == password:
+        del data[username]
+        write_data(data)
+
+
+#register('kuba', 'heslo', 'heslo')
+#delete_user('jana', 'heslo2')
+change_password('milan', 'heslo3', 'new_heslo', 'new_heslo')
