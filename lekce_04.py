7ca231af4f1fb86e8d1c7ef4f629db62fd4e69b1
Nick8755 <rocl.na85@gmail.com>
2024-11-19 19:10:30 +0100

    hash

--- lekce_04.py
+++ lekce_04.py
@@ -1,8 +1,15 @@
-from users import login, register
+import hashlib
+
+text = 'ohcoqgcoweg'
+hash = hashlib.sha256(text.encode()).hexdigest()
+print(hash)
+
+'''from users_change_password import login, register
 while True:
     username = input('Zadejte username: ')
     password = input('Zadejte heslo: ')
     success = login(username, password)
     if success:
         print('OK')
-        break
+    break
+'''
