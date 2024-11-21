87916f8945d48b2cda670569679c7c74137c4e26
Nikita_Abr <rock.na85@gmail.com>
2024-11-21 18:04:23 +0100

    okomentovani

--- ukol_02/ukol_02.py
+++ ukol_02/ukol_02.py
@@ -1,14 +1,14 @@
 file = 'file_02.txt'
-user_input = input('Zadejte text pro ulozeni do souboru: ')
+user_input = input('Zadejte text pro ulozeni do souboru: ') # Uživatel zadá text, který chce uložit do souboru
 
-def write():
+def write(): # Funkce, která uloží text do souboru
     with open('file_02.txt', 'a', encoding='utf-8') as file:
         file.write(user_input + '\n')
         print(f"Text '{user_input}' byl uložen do souboru.")
 
-def read():
+def read(): # Funkce, která načte text ze souboru
     with open('file_02.txt', 'r', encoding='utf-8') as file:
         print(file.read())
 
-write()
-read()
+write() # Zavolání funkce write()
+read()  # Zavolání funkce read()
