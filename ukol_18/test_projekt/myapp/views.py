bc9ce2c5dc827a39f363fff27ad4dded7bdb88d3
Nick8755 <rock.na85@gmail.com>
2025-01-28 16:47:48 +0100

    Zjištění věku, pokracovani

--- ukol_18/test_projekt/myapp/views.py
+++ ukol_18/test_projekt/myapp/views.py
@@ -3,21 +3,21 @@ from django.shortcuts import render
 
 
 def age(request):
-    message = ""
+    message = " "
 
     birth_year  = request.GET.get('birth_year', '')
     if birth_year.isdecimal():
         birth_year = int(birth_year)
         current_year = dt.datetime.now().year
-        age = current_year - birth_year
-        message = f"Je vám přibližně {age} let"
-
+        if 1900 <= birth_year <= current_year:
+            age = current_year - birth_year
+            message = f"Je vám přibližně {age} let"
+        else:
+            message = "Zadejte prosím rok narození v rozmezí od roku 1900 do současnosti."
     else:
         message = "Zadejte prosím rok narození jako celé číslo."
-
     context = {
-        'message': message
-
+            'message': message
     }
     return render(request, 'age.html', context)
 
